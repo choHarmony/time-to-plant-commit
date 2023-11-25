44ab8d84396f8fd49ce373b3a80b2f3a145404d1
choHarmony <jws0183@naver.com>
2023-11-25 15:57:58 +0900

    [Add] connect main viewmodel

--- app/src/main/java/com/example/gratify/view/MainActivity.kt
+++ app/src/main/java/com/example/gratify/view/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.gratify.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.gratify.R
+import com.example.gratify.databinding.ActivityMainBinding
+import com.example.gratify.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+        R.layout.activity_main)
+        binding.mainvm = MainViewModel()
     }
 }
